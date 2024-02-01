f6c140e062ddfa6decd972b5bbb4ad9fc2c6baa8
Jonathan Gainsley <38672162+gainsley@users.noreply.github.com>
2024-02-01 10:48:57 -0800

    create action

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # action-current-commit-is-tagged
+
+Checks if the current commit is tagged. This is useful for running a nightly
+test/build action that will tag the branch when done, or skip it if it's already tagged.
---
+++ current-commit-is-tagged.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get current commit
+current_commit=`git rev-parse HEAD`
+echo "current commit is $current_commit"
+
+# get tag for commit
+tags=`git tag --contains $current_commit`
+echo "tags for commit are $tags"
+
+if [[ -n "$tags" ]]; then
+    echo "current_commit_is_tagged=1" >> $GITHUB_OUTPUT
+else
+    echo "current_commit_is_tagged=0" >> $GITHUB_OUTPUT
+fi
